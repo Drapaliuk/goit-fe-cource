5f06f93d9260ef381b4acf7b79d2a7cd4c97a7f8
Vitalii <ghvitaliidrapaliuk@gmail,com>
2018-06-12 15:25:39 +0300

    HW1JS

---
+++ java-script/module-01/hw1.js
@@ -0,0 +1,44 @@
+// HOME WORK
+
+const cancel = 'Відмінено користувачем!';
+const accessDenied = 'Доступ обмежено!';
+const welcome = 'Ласкаво просимо!';
+let login;
+let password;
+
+const adminLogin = prompt ('Будь ласка введіть логін')
+if(adminLogin === 'ADMIN_LOGIN'){
+    const adminPassword = prompt ('Будь ласка введіть пароль')
+    if(adminPassword === 'ADMIN_PASSWORD'){
+        login = alert(welcome)
+    }else if(adminPassword === null){
+        login = alert(cancel)
+    }else{
+        login = alert(accessDenied)
+    }
+}else if(adminLogin === null){
+    password = alert(cancel)
+}else{
+    password = alert(accessDenied)
+}
+
+// EXTRA TASK
+let seats;
+const hurgada = 25;
+const taba = 6;
+const sharm = 15;
+const offer = prompt('Будь ласка виберіть набряк подорожі')
+if(offer === 'sharm'){
+    seats = prompt('Будь ласка введіть необхідну кількість місць')
+    }
+    if(seats <= sharm){
+    alert('Щасливої дороги!')
+    }else{
+    alert('Місць немає!')
+else if(offer === 'hurgada'){
+    seats = prompt('Будь ласка введіть необхідну кількість місць')
+    }
+    if(seats <= hurgada){
+    alert('Щасливої дороги!')
+    }else{
+    alert('Місць немає!')
