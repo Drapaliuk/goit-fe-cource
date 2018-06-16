c57a9a81a6867ce20ecdd94778c521526d29303b
Vitalii <ghvitaliidrapaliuk@gmail,com>
2018-06-16 18:11:57 +0300

    hw1

--- java-script/module-01/hw1.js
+++ java-script/module-01/hw1.js
@@ -3,13 +3,16 @@
 const cancel = 'Відмінено користувачем!';
 const accessDenied = 'Доступ обмежено!';
 const welcome = 'Ласкаво просимо!';
+const ADMIN_LOGIN = 'admin';
+const ADMIN_PASSWORD = 'm4ngo1zh4ackz0r'
 let login;
 let password;
 
+
 const adminLogin = prompt ('Будь ласка введіть логін')
-if(adminLogin === 'ADMIN_LOGIN'){
+if(adminLogin === ADMIN_LOGIN){
     const adminPassword = prompt ('Будь ласка введіть пароль')
-    if(adminPassword === 'ADMIN_PASSWORD'){
+    if(adminPassword === ADMIN_PASSWORD){
         login = alert(welcome)
     }else if(adminPassword === null){
         login = alert(cancel)
@@ -23,22 +26,22 @@ if(adminLogin === 'ADMIN_LOGIN'){
 }
 
 // EXTRA TASK
-let seats;
-const hurgada = 25;
-const taba = 6;
-const sharm = 15;
-const offer = prompt('Будь ласка виберіть набряк подорожі')
-if(offer === 'sharm'){
-    seats = prompt('Будь ласка введіть необхідну кількість місць')
-    }
-    if(seats <= sharm){
-    alert('Щасливої дороги!')
-    }else{
-    alert('Місць немає!')
-else if(offer === 'hurgada'){
-    seats = prompt('Будь ласка введіть необхідну кількість місць')
-    }
-    if(seats <= hurgada){
-    alert('Щасливої дороги!')
-    }else{
-    alert('Місць немає!')
+// let seats;
+// const hurgada = 25;
+// const taba = 6;
+// const sharm = 15;
+// const offer = prompt('Будь ласка виберіть набряк подорожі')
+// if(offer === 'sharm'){
+//     seats = prompt('Будь ласка введіть необхідну кількість місць')
+//     }
+//     if(seats <= sharm){
+//     alert('Щасливої дороги!')
+//     }else{
+//     alert('Місць немає!')
+// else if(offer === 'hurgada'){
+//     seats = prompt('Будь ласка введіть необхідну кількість місць')
+//     }
+//     if(seats <= hurgada){
+//     alert('Щасливої дороги!')
+//     }else{
+//     alert('Місць немає!')
