5727a38cc23ba8d4b9b2932184bd7fb22b18c929
Drapaliuk <ghvitaliidrapaliuk@gmail.com>
2018-07-24 09:04:20 +0300

    main

---
+++ java-script/module-02/mdl2main.js
@@ -0,0 +1,23 @@
+let userInput
+const numbers = []
+let total = 0;
+do{
+    userInput = Number(prompt('Введіть будь ласка число!'));
+    const revision = isNaN(userInput);
+    if(revision){
+        alert('Було введено не число, спробуйте ще раз')
+        continue
+    }
+    numbers.push(userInput)
+}while (userInput != 0);
+
+for(let key of numbers){
+    total += key;
+}
+
+if(numbers.length > 0){
+  alert(`Загальна сума дорівнює ${total}` );
+}
+
+
+console.log(1)
