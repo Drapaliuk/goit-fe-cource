e3c24a29db0684690cbc2e6bec3ea02259c30c5a
Drapaliuk <ghvitaliidrapaliuk@gmail.com>
2018-08-19 01:49:48 +0300

    f

---
+++ java-script/module-04/f.js
@@ -0,0 +1,51 @@
+const autoFillingIn = function(){
+    const bread = Number(prompt('Bread'))
+    const milk = Number(prompt('Milk'))
+    const apples = Number(prompt('Apples'))
+    const chicken = Number(prompt('Chicken'))
+    const cheese = Number(prompt('Cheese'))
+    const order = {
+        bread: bread,
+        milk: milk,
+        apples: apples,
+        cheese: cheese,
+        chicken: chicken,
+    }
+    console.log(order)
+    return order
+}
+const order = autoFillingIn()
+
+const{bread = 0, milk = 0, apples = 0, cheese = 0, chicken = 0} = order
+
+const products = {
+    bread: 10 * bread,
+    milk: 15 * milk,
+    apples: 20 * apples,
+    chicken: 50 * chicken,
+    cheese: 40 * cheese,
+};
+
+const getCustomerMoney = function() {
+    const howMm = Number(prompt('Скільки коштів у вас є в наявності?'));
+    return howMm
+}
+
+const burMon = getCustomerMoney()
+
+const countTotalPrice = function(order){
+    let total = 0;
+    for(const cost in order){
+        total += order[cost]
+    }
+    return total
+}
+const total = countTotalPrice(products)
+
+const change = function(buyerMoney, totalPrice){
+    const change = buyerMoney - totalPrice
+    if(change > -1){
+        return alert(`Вітаємо з покупкою, ваша решта становить ${change} гривень`)
+    }else return alert("У вас недостатньо коштів!")
+}
+change(burMon, total)
