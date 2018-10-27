90cd11241618c498436c8c7705660193f7b70166
Drapaliuk <ghvitaliidrapaliuk@gmail.com>
2018-10-27 17:31:24 +0300

    hw8

---
+++ java-script/module-08/mdl8.js
@@ -0,0 +1,35 @@
+const firstSmallImg = document.querySelector('.preview > li > img')
+
+const dataFullView = firstSmallImg.getAttribute('data-fullview')
+
+const fullView = document.querySelector('.fullview > img')
+
+fullView.setAttribute('src', dataFullView)
+
+const fullViewImg = fullView.firstElementChild
+
+
+const preview = document.querySelector('.preview')
+
+const gallery = Array.from(preview.children)
+
+const func = function(event){
+    
+    if(event.target.nodeName !== 'IMG') return
+    const x = event.target.getAttribute('data-fullview')
+    fullView.setAttribute('src', x)
+
+    const i = event.target.parentNode
+    
+    gallery.forEach(element => {
+        if(element.classList.contains('uniq'))
+            element.classList.remove('uniq')
+    });
+
+
+
+    i.classList.add('uniq')
+}
+
+
+preview.addEventListener('click', func)
